f33a57e3fc0ba8cb088c27ff033836cc86991a9a
Brennan Blair <bblair31@users.noreply.github.com>
2019-01-02 07:01:17 -0500

    update market status based upon current local time

--- src/components/MarketCard.js
+++ src/components/MarketCard.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+const checkMarketName = symbol => {
+  switch (symbol) {
+    case 'DIA':
+      return 'DOW'
+    case 'SPY':
+      return 'S&P 500'
+    case 'IWM':
+      return 'RUSSELL 2000'
+    default:
+      return symbol
+  }
+}
+
 const MarketCard = ({ market }) => {
   return (
     <div className="market-card" style={{display: "inline-block"}}>
-      <p>{market.companyName}</p>
+      <p>{checkMarketName(market.symbol)}</p>
       <p>${(market.latestPrice).toFixed(2)}</p>
+      <p>${(market.change).toFixed(2)}</p>
       <p>{(market.changePercent * 100).toFixed(2)}%</p>
     </div>
   )
--- src/components/MarketStatus.js
+++ src/components/MarketStatus.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const MarketStatus = (props) => {
-  return (
-    <div className="market-status">The Market is Currently OPEN
+const MarketStatus = () => {
+  let datetime = new Date()
+  let day = datetime.getDay()
+  let hour = datetime.getHours()
+  let minute = datetime.getMinutes()
 
-    </div>
-  )
+  if (day < 1 || day > 5) {
+    return <div className="market-status">Market is Currently CLOSED  </div>
+  } else if ((hour < 9 && minute < 30) || (hour > 16)) {
+    return <div className="market-status">Market is Currently CLOSED  </div>
+  } else {
+    return <div className="market-status">Market is Currently OPEN  </div>
+  }
 } /// End of Dashboard Class
 export default MarketStatus
