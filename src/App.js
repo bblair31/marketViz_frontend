ae57062bd113b34a6f6df37c9c1b5fdd582a0398
Brennan Blair <bblair31@users.noreply.github.com>
2019-01-16 17:08:54 -0500

    cleanup App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import Adapter from './apis/Adapter'
 import { Route, withRouter, Switch, Redirect } from 'react-router-dom'
 import Dashboard from './containers/Dashboard'
